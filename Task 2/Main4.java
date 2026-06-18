a330a71570a1135cecd7268468061f5ac9c35b60
Nabila Chowdhury <anjumnabila298@gmail.com>
2026-06-18 16:52:50 +0600

    Update Main4.java

--- Task 2/Main4.java
+++ Task 2/Main4.java
@@ -1,1 +1,31 @@
+class Sensor {
+    private String name;
+    private int batteryLevel;
+
+    public Sensor(String name, int batteryLevel) {
+        this.name = name;
+        this.batteryLevel = batteryLevel;
+
+    }
+    public void charge() {
+        this.batteryLevel = 100;
+        System.out.println("Sensor fully charged");
+    }
+    public int getBatteryLevel() {
+        return this.batteryLevel;
+    }
+    public String getName() {
+        return this.name;
+    }
+    
+}
+public class Main4 {
+    public static void main(String[] args) {
+        Sensor proximitySensor = new Sensor("Proximity sensor", 24);
+        System.out.println("Intial battery level for" + proximitySensor.getName() + ":" + proximitySensor.getBatteryLevel() + "%");
+        proximitySensor.charge();
+        System.out.println("Verified battery level:" + proximitySensor.getBatteryLevel() + "%");
+    }
+
+}
 
