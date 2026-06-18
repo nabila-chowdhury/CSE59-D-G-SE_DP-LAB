9103433ec2aa7fe08f21573c502d39286a89d7ee
Nabila Chowdhury <anjumnabila298@gmail.com>
2026-06-18 16:53:19 +0600

    Update Main5.java

--- Task 2/Main5.java
+++ Task 2/Main5.java
@@ -1,1 +1,17 @@
+class Printer {
+    public void printdata(String text) {
+        System.out.println("printing text:" + text);
+    }
+    public void printdata(int number) {
+        System.out.println("printing number:" + number);
+    }
+    
+}
+public class Main5{
+    public static void main(String[] args){
+        Printer myprinter = new Printer();
+        myprinter.printdata("hello world!");
+        myprinter.printdata(42);
+    }
+}
 
