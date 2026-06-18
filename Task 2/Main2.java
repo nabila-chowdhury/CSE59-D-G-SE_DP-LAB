7a8e6b89d229a257a7d1f3f9063a427de25ee969
Nabila Chowdhury <anjumnabila298@gmail.com>
2026-06-18 16:14:29 +0600

    Update Main2.java

--- Task 2/Main2.java
+++ Task 2/Main2.java
@@ -1,1 +1,18 @@
+class NumberChecker  {
+    public void check(int num) {
+        if (num % 2 == 0 && num > 10){
+            System.out.println("true");
+        } else {
+            System.out.println("flase");
+        }
+    }
+    
+}
 
+public class Main2 {
+    public static void main (String[] args) {
+        NumberChecker  checker = new NumberChecker ();
+
+        checker.check(14);
+    }
+}
