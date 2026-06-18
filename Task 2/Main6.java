b85af1cd18948c9e11e86f95d290d67c519dd2fc
Nabila Chowdhury <anjumnabila298@gmail.com>
2026-06-18 17:02:27 +0600

    Update Main6.java

--- Task 2/Main6.java
+++ Task 2/Main6.java
@@ -1,1 +1,27 @@
 
+class Applet {
+    public void launch() {
+        System.out.println("Applet launched");
+    }
+}
+
+
+class GameApplet extends Applet {
+    public void play() {
+        System.out.println("Playing game");
+    }
+}
+
+
+public class Main6 {
+    public static void main(String[] args) {
+        // Creating an object of the derived class
+        GameApplet myGame = new GameApplet();
+
+        // Calling the inherited method from the base class
+        myGame.launch();
+
+        // Calling the method from the derived class
+        myGame.play();
+    }
+}
