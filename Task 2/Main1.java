a45fbbe1f5d3aa6a21684af511b8d4d385d30efa
Nabila Chowdhury <anjumnabila298@gmail.com>
2026-06-18 16:02:32 +0600

    Update Main1.java

--- Task 2/Main1.java
+++ Task 2/Main1.java
@@ -1,1 +1,15 @@
+class ContestStats {
+    int[] scores = {85, 90, 95};
 
+    public void printFirstScore(){
+        System.out.println("The fisrt score is: " + scores[0]);
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        ContestStats stats = new ContestStats();
+
+        stats.printFirstScore();
+    }
+}
