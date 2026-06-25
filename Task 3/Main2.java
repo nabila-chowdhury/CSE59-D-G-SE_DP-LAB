83d91126f8ac9fe9595d9ddcbf71a48e82b08aa4
Nabila Chowdhury <anjumnabila298@gmail.com>
2026-06-25 16:58:27 +0600

    Update Main2.java

--- Task 3/Main2.java
+++ Task 3/Main2.java
@@ -1,1 +1,48 @@
+public interface Shape {
+    double calculateArea();
+}
+public class Rectangle implements Shape {
+    private final double length;
+    private final double width;
 
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+public class Circle implements Shape {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double calcukateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+public class Areacalculator {
+    public double calculeTotalArea(Shape[] shapes) {
+        double  totalArea = 0;
+        for (Shape shape : shapes) {
+            totalArea += shape.calculateArea();
+        }
+        return totalArea;
+    }
+}
+public class Triangle implements Shape {
+    private final double base;
+    private final double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+    public double calculateArea() {
+        return 0.5 * base * height;
+
+    }
